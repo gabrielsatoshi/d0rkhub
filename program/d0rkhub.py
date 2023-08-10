677531a1cc32711a221c2433dec64ecb3d4a0526
l1nu$ <112598996+gabrielsatoshi@users.noreply.github.com>
2023-08-10 11:34:26 -0300

    Update d0rkhub.py
    Big update script 2.0

--- program/d0rkhub.py
+++ program/d0rkhub.py
@@ -1,9 +1,25 @@
+################-IMPORTS-###################
+
+import sqlite3
 import requests
 from bs4 import BeautifulSoup
 from termcolor import colored
 import time
 
-#       enjoy :)
+###############DATA-CONECTION###############
+
+db_name = 'save_payloads'
+con = sqlite3.connect(db_name)
+
+###############CREATING TABLE###############
+
+con.execute('''CREATE TABLE IF NOT EXISTS urls
+               (id INTEGER PRIMARY KEY NOT NULL,
+                domain VARCHAR NOT NULL,
+                results VARCHAR NOT NULL);''')
+
+#################INTERFACE##################
+
 
 ascii_d0rkhub = '''
        /$$  /$$$$$$            /$$       /$$                 /$$      
@@ -22,19 +38,27 @@ print(colored('                       created by l1nu$ & artico','yellow'))
 print(colored('_________________________________________________________________________','blue'))
 print('')
 
+#################VIEW PAYLOADS##############
+
 see_dorks = str(input(colored('~ View payloads? ~ [y/n] ','red')))
-if(see_dorks == 'y'):
-    with open('dorks.txt', 'r') as d:
-        dork_file = d.read()
-        see_payloads = (colored(dork_file,'yellow'))
-        print('')
-        print('Payloads:'+see_payloads)
-else:
-    ('ok..')
+def view_payloads():
+    if(see_dorks == 'y'):
+        with open('dorks.txt', 'r') as d:
+            dork_file = d.read()
+            see_payloads = (colored(dork_file,'yellow'))
+            print('')
+            print('Payloads:'+see_payloads)
+    else:
+        ('ok..')
+view_payloads()
+
+###############CAPTURING DOMAIN#############
+
 print('')
 print(colored('~ Give me a target domain ~','magenta'))
 domain = input(colored('+ ','magenta'))
 
+##############COFIGURING URL################
 
 def google_search_urls(query):
     search_query = "https://www.google.com/search?q=" + query.replace(' ', '+')
@@ -55,17 +79,50 @@ def google_search_urls(query):
 
     return clean_urls
 
+##############GETTING THE PAYLOADS##########
+
 with open('dorks.txt', 'r') as d:
     dork_file = d.read()
 
-query = domain
+#########JOINING PAYLOADS TO DOMAIN#########
 
+query = domain
 search_urls = google_search_urls(dork_file + f' site: {domain}')
 print('')
-print(colored(f'Results for : "{domain}"','magenta'))
+print(colored(f'~ Results for : "{domain}" ~','magenta'))
+
+#########PRINTING QUERY RESULTS#############
 
 for line in search_urls:
     print(colored(line,'yellow'))
-
 print('')
+
+##########QUESTION SAVE RESULTS#############
+
+save_results = input(colored('~ Save results? ~','magenta'))
+print('')
+while save_results != 'y' and save_results != 'n':
+    save_results = input(colored('Try [y/n] :','red'))
+
+##########QUESTION CONDITIONAL##############
+
+if(save_results == 'y'):
+       
+    #######SAVING THE RESULTS ON DATABANK#######
+       
+    con.execute(f'INSERT INTO urls (domain, results) VALUES ("{domain}", "{search_urls}")')
+    con.commit()
+       
+    ########PRINTING DATABANK RESULTS###########
+       
+    print(colored(f'saved data in {db_name}.','green'))
+    for row in con.execute('SELECT * FROM urls '):
+        print(colored(row,'blue'))
+           
+    ##############CLOSING CONECTION#############
+       
+    con.close()
+else:
+    print('Bye..')
+
 
