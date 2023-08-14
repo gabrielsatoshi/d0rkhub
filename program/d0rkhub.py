2ee6ba0840ce66586b8a2e48d5fa6c89be68fb61
l1nu$ <112598996+gabrielsatoshi@users.noreply.github.com>
2023-08-14 15:44:32 -0300

    atualizacao

--- program/d0rkhub.py
+++ program/d0rkhub.py
@@ -1,8 +1,9 @@
 import sqlite3
 import requests
 from bs4 import BeautifulSoup
-from termcolor import colored
 import time
+import os
+os.system('color 3')
 
 db_name = 'save_payloads'
 con = sqlite3.connect(db_name)
@@ -12,12 +13,12 @@ con.execute('''CREATE TABLE IF NOT EXISTS urls
                 results VARCHAR NOT NULL);''')
 
 def delete_message():
-    symbol = colored('[!]','black','on_red')
-    message = colored('You will erase all data!.','black','on_yellow')
+    symbol = ('[!]')
+    message = ('You will erase all data!.')
     print(symbol+message)
 def success_message():
-    symbol = colored('[+]','black','on_light_grey')
-    message = colored('Success!','black','on_green')
+    symbol = ('[+]')
+    message = ('Success!')
     print(symbol+message)
 
 error = '[?]'
@@ -34,28 +35,31 @@ ascii_d0rkhub = '''
  \_______/ \______/ |__/      |__/  \__/|__/  |__/ \______/ |_______/  
 '''
 print('')
-print(colored('_________________________________________________________________________','blue'))
-print(colored(ascii_d0rkhub,'green'))
-print(colored('                       created by l1nu$ & artico','yellow'))
-print(colored('_________________________________________________________________________','blue'))
+print('_________________________________________________________________________')
+print(ascii_d0rkhub)
+print('                       created by l1nu$ & artico')
+print('_________________________________________________________________________')
 print('')
-#################INFORMATION################
-warning = (colored('[!]','red','on_red'))
-voidstring2 = '|'
-information = (colored('d0rkhub is a tool that should be used for academic purposes only\nit was not created to hurt or attack any government or institution.','black','on_dark_grey'))
-print(colored(warning+information))
-print(colored('https://github.com/gabrielsatoshi/d0rkhub','black'))
-#################VIEW PAYLOADS##############
-number1_menu = colored('[1]DorkHub','black','on_yellow')
-number2_menu = colored('[2]View data','black','on_cyan')
-number3_menu = colored('[3]Delete data','black','on_red')
-number4_menu = colored('[4]View payloads','black','on_blue')
-number5_menu = colored('[5]Exit ','black','on_dark_grey')
-voidstring = '|'
 
-print(number1_menu+number2_menu+number3_menu+number4_menu+number5_menu+voidstring)
+
+#################INFORMATION################
+warning = ('[!]')
+information = ('d0rkhub is a tool that should be used for academic purposes only\nit was not created to hurt or attack any government or institution.\n')
+print(warning+information)
+print('https://github.com/gabrielsatoshi/d0rkhub\n\n')
+#################VIEW PAYLOADS##############
+number1_menu = ('[1]DorkHub')
+number2_menu = ('[2]View data')
+number3_menu = ('[3]Delete data')
+number4_menu = ('[4]View payloads')
+number5_menu = ('[5]Exit ')
+
+print('_________________________________________________________________________')
+print(number1_menu+number2_menu+number3_menu+number4_menu+number5_menu)
 def main():
-    menu_q = int(input(colored('~ Chose a number ~ :','magenta')))
+    os.system('color 3')
+    print('')
+    menu_q = int(input('~ Chose a number ~ :'))
 
 
     def google_search_urls(query):
@@ -77,7 +81,7 @@ def main():
 
         return clean_urls
 
-    with open('program/payloads.txt', 'r') as d:
+    with open('payloads.txt', 'r') as d:
         dork_file = d.read()
 
 
@@ -85,44 +89,54 @@ def main():
         case 1:
             print('')
             time.sleep(1)
-            print(colored('~ Give me a target domain ~','magenta'))
-            domain = input(colored('+ ','magenta'))   
+            print('~ Give me a target domain ~')
+            domain = input('+ ')  
             query = domain
-            with open('program/payloads.txt', 'r') as d:
+            with open('payloads.txt', 'r') as d:
                 dork_file = d.read()
             search_urls = google_search_urls(dork_file + f' site: {domain}')
             print('')
-            print(colored(f'~ Results for : "{domain}" ~','magenta'))
+            print(f'~ Results for : "{domain}" ~')
             for line in search_urls:
-                print(colored(line,'yellow'))
+                print(line,'yellow')
             print('')
-            save_results = input(colored('~ Save results? [y/n] ~','magenta'))
+            save_results = input('~ Save results? [y/n] ~')
+            
             print('')
             while save_results != 'y' and save_results != 'n':
-                save_results = input(colored('Try [y/n] :','red'))
+                save_results = input('Try [y/n] :')
             if(save_results == 'y'):
+                os.system('color 6')
+                time.sleep(2)
                 con.execute(f'INSERT INTO urls (domain, results) VALUES ("{domain}", "{search_urls}")')
                 con.commit()
-                print(colored(f'saved in {db_name}.','green'))
+                print(f'saved in {db_name}.')
                 success_message()
                 for row in con.execute('SELECT * FROM urls '):
-                    print(colored(row,'blue'))
+                    print((row))
                 main()
                 con.close()
         case 2:
+            os.system('color 2')
+            time.sleep(2)
             con.execute('SELECT * FROM urls')
             if con.execute('SELECT COUNT(*) FROM urls').fetchone()[0] == 0:
-                print(colored('empty database.','red'))
+                os.system('color 4')
+                print('empty database.')
             else:
+                os.system('color 2')
+                time.sleep(2)
                 for row in con.execute('SELECT * FROM urls '):
-                    print(colored(row,'blue'))
+                    print(row)
             time.sleep(1)
             main()
         case 3:
+            os.system('color 4')
             delete_message()
-            confirm_delete = input(colored('[-]are you sure? [y/n]','black','on_yellow'))
+            confirm_delete = input('[-]are you sure? [y/n]')
             if (confirm_delete == 'y'):
-                print(colored('deleting...','red'))
+                os.system('color 2')
+                print('deleting...')
                 con.execute("drop table urls")
                 time.sleep(1)
                 con.execute('''CREATE TABLE IF NOT EXISTS urls
@@ -130,13 +144,15 @@ def main():
                         domain VARCHAR NOT NULL,
                         results VARCHAR NOT NULL);''')
             else:
-                print(colored('canceling operation...','green'))
+                os.system('color 2')
+                print('canceling operation...')
                 time.sleep(1)
             main()
         case 4:
-            with open('program/payloads.txt', 'r') as d:
+            os.system('color 6')
+            with open('payloads.txt', 'r') as d:
                 dork_file = d.read()
-                see_payloads = (colored(dork_file,'yellow'))
+                see_payloads = (dork_file)
                 print('')
                 print('Payloads:'+see_payloads)
                 print('')
---
+++ program/start dorkhub.bat
@@ -0,0 +1,1 @@
+python d0rkhub.py
