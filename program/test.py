2bc5f292d99e072b95238c72876f681830c70d93
l1nu$ <112598996+gabrielsatoshi@users.noreply.github.com>
2023-08-24 21:25:26 -0300

    Update test.py

--- program/test.py
+++ program/test.py
@@ -1,17 +1,26 @@
-import requests
-from bs4 import BeautifulSoup
+#import requests
+#from bs4 import BeautifulSoup
 
 # URL target
-url = "http://www.teakstore.com.br/produtos.php?cat=%27"
+#url = "http://www.teakstore.com.br/produtos.php?cat=%27"
 
 # Faça uma requisição HTTP para obter os dados da página
-response = requests.get(url)
+#response = requests.get(url)
 
 # Extraia o conteúdo da página
-html_text = response.text
-soup = BeautifulSoup(html_text, "html.parser")
+#html_text = response.text
+#soup = BeautifulSoup(html_text, "html.parser")
 
 # Encontre a <body> e extraia o conteúdo da página
-content = soup.body.text
+#content = soup.body.text
 
-print(content)
+#print(content)
+from googlesearch import search
+
+results = search("Google", num_results=25)
+results_list = list(results)
+def print_top_100_results(results):
+    for i in range(25):
+        print(results[i].text)
+
+print_top_100_results(results_list)
